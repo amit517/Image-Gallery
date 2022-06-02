9b10636341753a0f4a1c28bace393149318cdae0
Amit Kundu <kundu.amit517@gmail.com>
2022-06-02 10:11:26 +0600

    feat: empty gallery and image details screen added

---
+++ app/src/main/java/com/assessment/mobileengineerassesment/view/gallery/GalleryFragment.kt
@@ -0,0 +1,16 @@
+package com.assessment.mobileengineerassesment.view.gallery
+
+import com.assessment.base.view.BaseFragment
+import com.assessment.base.viewmodel.BaseViewModel
+import com.assessment.mobileengineerassesment.R
+import com.assessment.mobileengineerassesment.databinding.FragmentGarreryBinding
+
+class GalleryFragment : BaseFragment<FragmentGarreryBinding>() {
+    override fun layoutId(): Int = R.layout.fragment_garrery
+
+    override fun getViewModel(): BaseViewModel? = null
+
+    override fun initOnCreateView() {
+
+    }
+}
---
+++ app/src/main/java/com/assessment/mobileengineerassesment/view/imagedetails/ImageDetailsFragment.kt
@@ -0,0 +1,16 @@
+package com.assessment.mobileengineerassesment.view.imagedetails
+
+import com.assessment.base.view.BaseFragment
+import com.assessment.base.viewmodel.BaseViewModel
+import com.assessment.mobileengineerassesment.R
+import com.assessment.mobileengineerassesment.databinding.FragmentImageDetailsBinding
+
+class ImageDetailsFragment : BaseFragment<FragmentImageDetailsBinding>() {
+    override fun layoutId(): Int = R.layout.fragment_image_details
+
+    override fun getViewModel(): BaseViewModel? = null
+
+    override fun initOnCreateView() {
+
+    }
+}
