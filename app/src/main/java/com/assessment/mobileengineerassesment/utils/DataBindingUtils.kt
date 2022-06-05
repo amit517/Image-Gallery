17fae4bd95a4ecfbb6ede21b5490420d22f2c876
Amit Kundu <kundu.amit517@gmail.com>
2022-06-05 23:46:05 +0600

    perf: glide disk cache strategy updated
    loaded smaller image on the gallery list image

--- app/src/main/java/com/assessment/mobileengineerassesment/utils/DataBindingUtils.kt
+++ app/src/main/java/com/assessment/mobileengineerassesment/utils/DataBindingUtils.kt
@@ -8,6 +8,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.assessment.mobileengineerassesment.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
@@ -33,6 +34,7 @@ fun ImageView.loadImage(url: String?, color: String?) {
                 .load(Uri.parse(url))
                 .transform(CenterInside(), RoundedCorners(16))
                 .timeout(60 * 1000)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .placeholder(ColorDrawable(Color.parseColor(color)))
                 .error(R.drawable.image_place_holder)
                 .into(this)
