37c876fcbdeae276b54780c827a5390e974c0e85
Amit Kundu <kundu.amit517@gmail.com>
2022-06-05 23:40:37 +0600

    refactor: refactor base application and some other classes

--- app/src/main/java/com/assessment/mobileengineerassesment/view/gallery/ImagePagingSource.kt
+++ app/src/main/java/com/assessment/mobileengineerassesment/view/gallery/ImagePagingSource.kt
@@ -2,8 +2,8 @@ package com.assessment.mobileengineerassesment.view.gallery
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.assessment.base.utils.MsgUtil.networkErrorMsg
 import com.assessment.base.utils.MsgUtil.somethingWentWrong
-import com.assessment.mobileengineerassesment.MsgUtil.networkErrorMsg
 import com.assessment.mobileengineerassesment.model.ImageResponse
 import com.assessment.mobileengineerassesment.model.ImageSearchQuery
 import com.assessment.mobileengineerassesment.network.UnsplashApiService
@@ -14,7 +14,7 @@ import java.io.IOException
 class ImagePagingSource(
     private val imageSearchQuery: ImageSearchQuery,
     private val apiService: UnsplashApiService,
-    private val pageSize : Int
+    private val pageSize: Int,
 ) : PagingSource<Int, ImageResponse>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ImageResponse> {
@@ -22,7 +22,7 @@ class ImagePagingSource(
         val position: Int = params.key ?: FIRST_PAGE_ITEM
 
         val response: GenericResponse<List<ImageResponse>> =
-            apiService.getImageCollection(position,pageSize,imageSearchQuery.orderBy)
+            apiService.getImageCollection(position, pageSize, imageSearchQuery.orderBy)
 
         return when (response) {
             is BaseResponse.Success -> {
@@ -55,7 +55,7 @@ class ImagePagingSource(
     companion object {
         const val FIRST_PAGE_ITEM = 1
     }
-    
+
     override fun getRefreshKey(state: PagingState<Int, ImageResponse>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
--- base/src/main/java/com/assessment/base/utils/MsgUtil.kt
+++ base/src/main/java/com/assessment/base/utils/MsgUtil.kt
@@ -2,4 +2,5 @@ package com.assessment.base.utils
 
 object MsgUtil {
     const val somethingWentWrong = "Something went wrong"
+    const val networkErrorMsg = "Please check your internet connection"
 }
